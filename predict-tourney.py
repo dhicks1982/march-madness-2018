6df36f65dc68a188afc32db7a3d89e391e0206f8
David Hicks <david@Davids-MacBook-Pro.local>
2018-03-14 23:51:42 -0400

    Submission for 2018

--- predict-tourney.py
+++ predict-tourney.py
@@ -1,5 +1,6 @@
 import csv
 from operator import attrgetter
+import math
 
 resultsDict = {}
 teamsDict = {}
@@ -8,7 +9,7 @@ regularSeasonResults = {}
 yearTeamSeed = {}
 # dataDir = '/vagrant/spark/data/'
 # dataDir = '/Users/david/sila/marchmadness/data/'
-dataDir = '/Users/david/Downloads/'
+dataDir = '/Users/david/sila/marchmadness/2018/'
 
 with open(dataDir+'RegularSeasonCompactResults.csv') as seasonResultsFile:
     seasonResultsReader = csv.reader(seasonResultsFile, delimiter=',')
@@ -47,7 +48,6 @@ with open(dataDir+'RegularSeasonCompactResults.csv') as seasonResultsFile:
             })
         rsry[lteam]['opponents'].add(wteam)
 
-
 with open(dataDir+'Teams.csv') as teamsFile:
     teamsReader = csv.reader(teamsFile, delimiter=',')
     teamsReader.next() # skip header
@@ -55,7 +55,7 @@ with open(dataDir+'Teams.csv') as teamsFile:
         teamsDict[teamsRow[0]] = teamsRow[1]
 
 
-with open(dataDir+'TourneyCompactResults.csv') as resultsFile:
+with open(dataDir+'NCAATourneyCompactResults.csv') as resultsFile:
     resultsReader = csv.reader(resultsFile, delimiter=',')
     resultsReader.next() # skip header
     for result in resultsReader:
@@ -66,7 +66,7 @@ with open(dataDir+'TourneyCompactResults.csv') as resultsFile:
         resultsDict[year][wteam] = resultsDict[year].get(wteam,set())
         resultsDict[year][wteam].add(lteam)
 
-with open(dataDir+'TourneySeeds.csv') as seedsFile:
+with open(dataDir+'NCAATourneySeeds.csv') as seedsFile:
     seedsReader = csv.reader(seedsFile, delimiter=',')
     seedsReader.next()
     seedMap = [1,16,8,9,4,13,5,12,3,14,6,11,7,10,2,15]
@@ -84,7 +84,10 @@ with open(dataDir+'TourneySeeds.csv') as seedsFile:
         yearTeamSeed[year][team]=seed_int
         row = (row+1)%4 if seed_int is 1 else row
         if playIn:
-            seedsDict[year][row*16+index] = team if team in resultsDict[year] else playInTeam
+            if year in resultsDict:
+                seedsDict[year][row*16+index] = team if team in resultsDict[year] else playInTeam
+            else:
+                seedsDict[year][row*16+index] = team
             playIn = False
         elif seed.endswith('a'):
             playIn = True
@@ -168,33 +171,39 @@ def updateSecondAndThirdOrder(team, statName, ratingName, oppRatingName, oppOppR
     team['secondOrder'+statName] = team[ratingName]/2.0+team[oppRatingName]/4.0
     team['thirdOrder'+statName] = team[ratingName]/2.0+team[oppRatingName]/4.0+team[oppOppRatingName]/8.0
 
+def scaleDay(daynum):
+    return math.sqrt(daynum)
+
 def calculateStatistics(rsry):
     for teamId, team in rsry.items():
         daySum = 0
         for game in team['games']:
-            daySum = daySum+game['daynum']
+            dayMultiplier = scaleDay(game['daynum'])
+            daySum = daySum+dayMultiplier
             game['rawRating'] = game['score']/float(game['score']+game['oscore'])
             game['pythRating'] = float(game['score']*game['score'])/float(game['score']*game['score']+game['oscore']*game['oscore'])
             team['rawRatingSum'] = (team.get('rawRatingSum',0.0)+game['rawRating'])
             team['pythRatingSum'] = (team.get('pythRatingSum',0.0)+game['pythRating'])
-            team['rawRatingSumDayWeight'] = team.get('rawRatingSumDayWeight',0.0)+game['rawRating']*game['daynum']
-            team['pythRatingSumDayWeight'] = team.get('pythRatingSumDayWeight',0.0)+game['pythRating']*game['daynum']
+            team['rawRatingSumDayWeight'] = team.get('rawRatingSumDayWeight',0.0)+game['rawRating']*dayMultiplier
+            team['pythRatingSumDayWeight'] = team.get('pythRatingSumDayWeight',0.0)+game['pythRating']*dayMultiplier
 
         team['daySum'] = daySum
         team['rawRatingAvg'] = team['rawRatingSum']/len(team['games'])
         team['pythRatingAvg'] = team['pythRatingSum']/len(team['games'])
         team['rawRatingAvgDayWeight'] = team['rawRatingSumDayWeight']/daySum
         team['pythRatingAvgDayWeight'] = team['pythRatingSumDayWeight']/daySum
+
     for teamId, team in rsry.items():
         uniqueOppOfOppGameCount = 0
         uniqueOppOfOppDaySum = 0
         for game in team['games']:
+            dayMultiplier = scaleDay(game['daynum'])
             oppRawRating = rsry[game['opp']]['rawRatingAvg']
             oppPythRating = rsry[game['opp']]['pythRatingAvg']
             team['oppRawRatingSum'] = team.get('oppRawRatingSum',0.0)+oppRawRating
             team['oppPythRatingSum'] = team.get('oppPythRatingSum',0.0)+oppPythRating
-            team['oppRawRatingSumDayWeight'] = team.get('oppRawRatingSumDayWeight',0.0)+oppRawRating*game['daynum']
-            team['oppPythRatingSumDayWeight'] = team.get('oppPythRatingSumDayWeight',0.0)+oppPythRating*game['daynum']
+            team['oppRawRatingSumDayWeight'] = team.get('oppRawRatingSumDayWeight',0.0)+oppRawRating*dayMultiplier
+            team['oppPythRatingSumDayWeight'] = team.get('oppPythRatingSumDayWeight',0.0)+oppPythRating*dayMultiplier
 
             team['oppAgainstNonOppSum'] = team.get('oppAgainstNonOppSum',0.0)
             oneOppSum = 0.0
@@ -205,9 +214,9 @@ def calculateStatistics(rsry):
                     oneOppSum += oppGame['rawRating']
             if count > 0:
                 uniqueOppOfOppGameCount+=1
-                uniqueOppOfOppDaySum += game['daynum']
+                uniqueOppOfOppDaySum += dayMultiplier
                 team['oppAgainstNonOppSum'] = team.get('oppAgainstNonOppSum',0.0)+oneOppSum/count
-                team['oppAgainstNonOppSumDayWeight'] = team.get('oppAgainstNonOppSumDayWeight',0.0)+(oneOppSum/count)*game['daynum']
+                team['oppAgainstNonOppSumDayWeight'] = team.get('oppAgainstNonOppSumDayWeight',0.0)+(oneOppSum/count)*dayMultiplier
 
         team['oppRawRatingAvg'] = team['oppRawRatingSum']/len(team['games'])
         team['oppRawRatingAvgDayWeight'] = team['oppRawRatingSumDayWeight']/team['daySum']
@@ -217,14 +226,15 @@ def calculateStatistics(rsry):
         team['oppAgainstNonOppAvgDayWeight'] = team['oppAgainstNonOppSumDayWeight']/uniqueOppOfOppDaySum
     for teamId, team in rsry.items():
         for game in team['games']:
+            dayMultiplier = scaleDay(game['daynum'])
             oppOppRawRating = rsry[game['opp']]['oppRawRatingAvg']
             oppOppPythRating = rsry[game['opp']]['oppPythRatingAvg']
             team['oppOppRawRatingSum'] = team.get('oppOppRawRatingSum',0.0)+oppOppRawRating
             team['oppOppPythRatingSum'] = team.get('oppOppPythRatingSum',0.0)+oppOppPythRating
-            team['oppOppRawRatingSumDayWeight'] = team.get('oppOppRawRatingSumDayWeight',0.0)+oppOppRawRating*game['daynum']
-            team['oppOppPythRatingSumDayWeight'] = team.get('oppOppPythRatingSumDayWeight',0.0)+oppOppPythRating*game['daynum']
+            team['oppOppRawRatingSumDayWeight'] = team.get('oppOppRawRatingSumDayWeight',0.0)+oppOppRawRating*dayMultiplier
+            team['oppOppPythRatingSumDayWeight'] = team.get('oppOppPythRatingSumDayWeight',0.0)+oppOppPythRating*dayMultiplier
             team['oppOppAgainstNonOppSum'] = team.get('oppOppAgainstNonOppSum',0.0)+rsry[game['opp']]['oppAgainstNonOppAvg']
-            team['oppOppAgainstNonOppSumDayWeight'] = team.get('oppOppAgainstNonOppSumDayWeight',0.0)+rsry[game['opp']]['oppAgainstNonOppAvg']*game['daynum']
+            team['oppOppAgainstNonOppSumDayWeight'] = team.get('oppOppAgainstNonOppSumDayWeight',0.0)+rsry[game['opp']]['oppAgainstNonOppAvg']*dayMultiplier
 
         team['oppOppRawRatingAvg'] = team['oppOppRawRatingSum']/len(team['games'])
         team['oppOppPythRatingAvg'] = team['oppOppPythRatingSum']/len(team['games'])
@@ -259,7 +269,7 @@ scoresByAlgorithm = {}
 stats = ['pythRatingAvg','pythRatingAvgDayWeight','rawRatingAvg','rawRatingAvgDayWeight','oppOppRawRatingAvg','oppOppPythRatingAvg','oppOppRawRatingAvgDayWeight','oppOppPythRatingAvgDayWeight','secondOrderRaw','thirdOrderRaw','secondOrderRawByDay','thirdOrderRawByDay','secondOrderPyth','secondOrderPythByDay','thirdOrderPyth','thirdOrderPythByDay','secondOrderRawFancy','thirdOrderRawFancy','secondOrderRawFancyByDay','thirdOrderRawFancyByDay']
 for year, seeds in seedsDict.items():
     calculateStatistics(regularSeasonResults[year])
-    if year != '2017':
+    if year != '2018':
         boring = calculateTournament(seeds,calculateGame)
         rg = ResultGetter(resultsDict[year])
         actual = calculateTournament(seeds,rg.calculateGame)
@@ -281,7 +291,7 @@ printScoresForSystem('weightedWithBonus')
 
 
 stat = 'thirdOrderRawFancyByDay'
-year = '2017'
+year = '2018'
 advancedStatsCalculator = AdvancedStats(regularSeasonResults[year],stat)
 bracket = calculateTournament(seedsDict[year],advancedStatsCalculator.calculateGame)
 
